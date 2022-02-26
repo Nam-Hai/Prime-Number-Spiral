411db617d8dc233e4ea959b430f0e62c4d2a3839
BlueTompon <lucastran00@gmail.com>
2022-02-26 18:29:27 +0100

    prime

--- sketch.js
+++ sketch.js
@@ -1,9 +1,70 @@
+const stepSize = 10;
+const r = 4;
+let curN = 1;
+let stepIndex = 0;
+let stepLenght = 1;
+let rotateIndex = 1;
+let x = 0;
+let y = 0;
+
 function setup() {
-  createCanvas(400, 300);
+  createCanvas(500, 500);
+  background(220);
 
 }
 
 function draw() {
-  background(220);
+  translate(width / 2, height / 2)
+  fill(255);
 
+  if (isPrime(curN)) {
+    circle(x, y, r);
+  }
+
+  switch (rotateIndex % 4) {
+    case 1:
+      line(x, y, x + stepSize, y)
+      x += stepSize;
+      break
+    case 2:
+      line(x, y, x, y - stepSize)
+      y -= stepSize;
+      break
+    case 3:
+      line(x, y, x - stepSize, y)
+      x -= stepSize;
+      break
+    case 0:
+      line(x, y, x, y + stepSize)
+      y += stepSize;
+      break
+  }
+
+
+
+  stepIndex++;
+  curN++;
+  if (stepIndex == stepLenght) {
+    stepIndex = 0;
+    rotateIndex++;
+    if (rotateIndex % 2 == 1) {
+      stepLenght += 1;
+    }
+  }
+
+  if (curN == 2700) {
+    noLoop()
+  }
+}
+
+
+
+function isPrime(n) {
+  for (let i = 2; i <= sqrt(n); i++) {
+    if (n % i == 0) {
+      return false
+    }
+  }
+
+  return n > 1
 }
